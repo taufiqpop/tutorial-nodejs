4ec2837635177ab2e1af2e9cf0cc922d6f9617a6
taufiqpop99 <taufiqpop999@gmail.com>
2024-02-09 11:10:36 +0700

    module

--- 06-nodejs-module-system/coba.js
+++ 06-nodejs-module-system/coba.js
@@ -2,4 +2,32 @@ function cetakNama (nama) {
     return `Halo, nama saya ${nama}`;
 }
 
-module.exports = cetakNama;
+const PI = 3.14;
+
+const mahasiswa = {
+    nama : 'Taufiq Pop',
+    umur : 22,
+    cetakMhs(){
+        return `Halo, nama saya ${this.nama} dan saya ${this.umur} tahun.`;
+    }
+};
+
+class Orang {
+    constructor() {
+        console.log('Objek orang telah dibuat!');
+    }
+}
+
+// module.exports.cetakNama = cetakNama;
+// module.exports.PI = PI;
+// module.exports.mahasiswa = mahasiswa;
+// module.exports.Orang = Orang;
+
+// module.exports = {
+//     cetakNama: cetakNama,
+//     PI: PI,
+//     mahasiswa: mahasiswa,
+//     Orang: Orang
+// };
+
+module.exports = { cetakNama, PI, mahasiswa, Orang };
--- 06-nodejs-module-system/index.js
+++ 06-nodejs-module-system/index.js
@@ -1,5 +1,14 @@
-const fs = require('fs'); // Core Module
-const cetakNama = require('./coba'); // Local Module
-const moment = require('moment'); // Third Party Module / NPM Module / node_modules
+// Penjelasan Module System
 
-console.log(cetakNama('Taufiq Pop'));
+// const fs = require('fs'); // Core Module
+// const cetakNama = require('./coba'); // Local Module
+// const moment = require('moment'); // Third Party Module / NPM Module / node_modules
+
+const coba = require('./coba');
+
+console.log(
+    coba.cetakNama('Taufiq Pop'), 
+    coba.PI, 
+    coba.mahasiswa.cetakMhs(), 
+    new coba.Orang
+);
