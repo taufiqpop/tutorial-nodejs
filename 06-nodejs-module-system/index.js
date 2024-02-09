047fe3934acae507e793c05dde0bbe49592a1b5e
taufiqpop99 <taufiqpop999@gmail.com>
2024-02-09 10:33:41 +0700

    belajar-nodejs

---
+++ 06-nodejs-module-system/index.js
@@ -0,0 +1,5 @@
+const fs = require('fs'); // Core Module
+const cetakNama = require('./coba'); // Local Module
+const moment = require('moment'); // Third Party Module / NPM Module / node_modules
+
+console.log(cetakNama('Taufiq Pop'));
