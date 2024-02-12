48a2d329108aa1049f083014e878edc1366edda6
taufiqpop99 <taufiqpop999@gmail.com>
2024-02-12 16:26:27 +0700

    nodemon

--- 08-npm/app.js
+++ 08-npm/app.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const validator = require('validator');
+// console.log(validator.isEmail('taufiqpop999@gmail.com'));
+// console.log(validator.isMobilePhone('08123456789', 'id-ID'));
+// console.log(validator.isNumeric('0812345678a9'));
+
+const chalk = require('chalk');
+console.log(chalk.italic.bgRed('Hello World!'));
+const pesan = chalk`Keep Calm and {bgBlue.black.bold Chill}, but {bgGreen.italic.white Unstoppable}`;
+console.log(pesan);
